c2a84b96239acbb780109006a91277193e874967
yufeng wan <yufeng.wan@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:45:03 +0100

    presque moitié de l exo56

--- src/main.c
+++ src/main.c
@@ -1,10 +1,28 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
 
 int main(void)
 {
-	tooti
-    return 0;
+    int seuil;
+    int i;
+    unsigned char bin[10];
+	
+    seuil = 9;
+    bin [10]= {0,1,2,3,4,5,6,7,8,9}
+	for (i=0 ; i<10 ; i++)
+    {
+                          if (bin[10]<seuil)
+    {
+        bin[i] = 0 ;
+                          }
+                                   else
+                          {
+                              bin[i] = 1 ;
+                          }
+	 return 0;
 }
+
+int main (void)
+{
+    unsigned char sat[10] = {1, 5, 9, 11, 4, 3, 88, 14, 26, 47};
+    signed SEUILH = 200;
+    signed SEUILB = -100;
