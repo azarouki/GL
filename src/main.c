2446ea498197460abd5550c30e7d99e25cfb67aa
azarouki <44802308+azarouki@users.noreply.github.com>
2018-11-21 22:38:23 +0100

    Update main.c
    j ai plus CVI du coup j ai fait sur sublime text

--- src/main.c
+++ src/main.c
@@ -5,24 +5,92 @@ int main(void)
     int seuil;
     int i;
     unsigned char bin[10];
-	
+   
     seuil = 9;
+
     bin [10]= {0,1,2,3,4,5,6,7,8,9}
-	for (i=0 ; i<10 ; i++)
+
     {
-                          if (bin[10]<seuil)
-    {
-        bin[i] = 0 ;
+if (bin[10]<seuil)
+ {
+    bin[i] = 0 ;
                           }
                                    else
                           {
-                              bin[i] = 1 ;
+                                     bin[i] = 1 ;
                           }
-	 return 0;
+    return 0;
 }
+
 
 int main (void)
 {
     unsigned char sat[10] = {1, 5, 9, 11, 4, 3, 88, 14, 26, 47};
-    signed SEUILH = 200;
-    signed SEUILB = -100;
+    short seuil = 155; 
+    SEUILH =200;
+    SEUILB=-100;
+    i,i2,i3,h,i4,i5;
+    short sat[10];
+    a[5]={1,5,9,8,11},b[5];
+ 
+int main(void)
+{
+for (i=0 ; i<10 ;i=i+1)
+{
+if (bin[i] >= seuil )
+{
+         bin[i] =1;
+}
+         else 
+{
+         bin[i] =0;
+}
+
+
+for (i2=0 ; i2<10 ;i2=i2+1)
+{
+
+         sat[i2]=rand()%400-150;
+
+if (sat[i2]<SEUILB)
+{
+         sat[i2] = SEUILB;
+}
+
+else if(sat[i2]>SEUILH)
+{
+         sat[i2] = SEUILH; 
+}
+}
+
+for (i3=0 ; i3<10 ;i3=i3+1)
+{
+
+         h=i3-1; 
+if (i3>0)
+{
+         h=i3-1;
+while(h>=0)
+{
+         dist[i3]=dist[i3]-dist[h];
+         h=h-1;
+}
+}
+else
+{
+         dist[i3]=dist[i3];
+}
+}
+for(i4=0;i4<5;i4=i4+1)
+{
+         b[i4]=a[4-i4];
+}
+for(i5=0;i5<3;i5=i5+1)
+{
+         miroir[i5+3]=miroir[2-i5];
+}
+return 0;
+         
+}
+}
+ 
